b75d02842e91ac3264602742b08536b3e517ab7d
Gabriel Gimenez Catuogno <12-11006@usb.ve>
2017-09-08 13:42:54 +0200

    Add functionality to watermark and watermark Image

--- PSG-Watermarks/watermarkimage.py
+++ PSG-Watermarks/watermarkimage.py
@@ -1,28 +1,28 @@
+from PIL import Image
 
-from PIL import ImageOps
-
-# The watermark image
 
 class WatermarkImage:
-    
+    # The watermark image
+
     def __init__(self, image_path):
         self.image_path = image_path
-        self.image = None
-    
+        self.image = Image.open(image_path)
+
     # Rotate the image
     # angle - float, eg. 75.5
     def rotate(self, angle):
         pass
-    
+
     # Apply a filter to image
     # image_filter - one of the ImageFilter constants
-    # eg. ImageFilter.CONTOUR 
+    # eg. ImageFilter.CONTOUR
     def transform(self, image_filter):
         pass
-    
 
 
 def main():
+    pass
+
 
 if __name__ == '__main__':
     main()
